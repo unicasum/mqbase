2f5404a80b5694bacec4c4872c8e5d5913c5fc20
Markus Felten <Markus.Felten@gmx.de>
2015-12-11 20:19:56 +0100

    feat: featch for releases

--- app/routes/environments.js
+++ app/routes/environments.js
@@ -1,13 +1,19 @@
 import Ember from 'ember';
+import fetch from 'fetch';
 
 export default Ember.Route.extend({
   model() {
+    return fetch('data/environments.json').then(function (response) {
+      return response.json().then(json => json.environments);
+    });
+
+    /*
     return [{
       "name": "CIT1"
     },{
       "name": "SIT1"
     }, {
       "name": "PRD1"
-    }];
+    }];*/
   }
 });
--- app/routes/releases.js
+++ app/routes/releases.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
+import fetch from 'fetch';
 
 export default Ember.Route.extend({
 
   model() {
+    return fetch('data/releases.json').then(function (response) {
+      return response.json().then(json => json.releases);
+    });
+    /*
     return [{
       "name": "R1",
       "description": "description 1"
@@ -10,6 +15,7 @@ export default Ember.Route.extend({
       "name": "R2",
       "description": "description 2"
     }];
+    */
   }
 
 });
