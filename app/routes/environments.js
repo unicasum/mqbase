9a3dc61616a27126870319e206f76205f8117bf2
Markus Felten <Markus.Felten@gmx.de>
2015-12-12 11:55:05 +0100

    outdated && upToDate

--- app/routes/environments.js
+++ app/routes/environments.js
@@ -1,10 +1,23 @@
 import Ember from 'ember';
 import fetch from 'fetch';
 
+
+function makeEnvironment(json) {
+  return {
+    name: json.name,
+    state: json.state,
+    get outdated() {
+      return this.state === 'outdated';
+    },
+    get upToDate() {
+      return this.state === 'up-to-date';
+    }
+  };
+}
+
 export default Ember.Route.extend({
   model() {
-    return fetch('data/environments.json').then(function (response) {
-      return response.json().then(json => json.environments);
-    });
+    return fetch('data/environments.json').then(response => response.json().then(json => json.environments.map(e =>
+      makeEnvironment(e))));
   }
 });
